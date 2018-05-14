369a8a50348d380f34812e5d6ed02227c84f2d99
npole0103 <npole0103@naver.com>
2018-05-14 22:06:58 +0900

    add calculator

--- calculator.c
+++ calculator.c
@@ -2,19 +2,19 @@
 
 // function that add integers a and b
 int iAdd(int a, int b) {
-    return a+b;
+    return a+b; //add
 }
 
 // function that subtract integers a and b
 int iSub(int a, int b) {
-    return a-b;
+    return a-b; //sub
 }
 
 // function that multiply integers a and b
 int iMul(int a, int b) {
-    return a*b;
+    return a*b; //mul
 }
 
 int iDiv(int a, int b) {
-    return a/b;
+    return a/b; //div
 }
